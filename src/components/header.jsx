b74e4681e08c53bd81da68097dd15d18a1a2112e
Vasanth Balasubramaniyan <vambuvasanth124@gmail.com>
2025-06-29 20:21:57 +0530

    Fix: file not exist

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,10 +8,15 @@ export default function Header() {
         const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
         const year = now.getFullYear();
 
-        const timestamp = `${day}-${month}-${year}`
+        // Format time to HH-MM-SS
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        const timestamp = `${day}-${month}-${year}_${hours}-${minutes}-${seconds}`;
 
         const link = document.createElement("a");
-        link.href = "/public/Vasanth B.pdf"
+        link.href = "/Vasanth B.pdf"
         link.download = `Vasanth B ${timestamp}.pdf`;
         link.click();
     }
